8e490411a43b882bd9081e23e2c6508720436a5a
sami <samialabdalah@yahoo.com>
2023-08-17 20:18:57 +0200

    work with entity

--- Models/Entities/UserProfileEntity.cs
+++ Models/Entities/UserProfileEntity.cs
@@ -8,14 +8,11 @@ public class UserProfileEntity
 {
     [Key, ForeignKey("User")]
     public string UserId { get; set; } = null!;
-
     public string? FirstName { get; set; } = null!;
     public string? LastName { get; set; } = null!;
     public string? StreetName { get; set; } 
     public string? PostalCode { get; set; } 
     public string? City { get; set; }
-
-    //Optional
     public string? CompanyName { get; set; }
    
     public IdentityUser User { get; set; } = null!;
